4a23cad7c3527ea70ed97d153500254404ea03fc
Star <125512934+StaryLOL@users.noreply.github.com>
2024-04-21 15:31:38 -0700

    Update mainscript.lua

--- mainscript.lua
+++ mainscript.lua
@@ -1,24 +1,14 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/StaryLOL/Levitate/main/bin/antikick.lua'))()) -- bridgeduels kick bypass
+loadstring(game:HttpGet("https://raw.githubusercontent.com/StaryLOL/Levitate/main/bin/antikick.lua", true))()
    wait(1.5)
 
 if game.PlaceId == 8542259458 or game.PlaceId == 8542275097 or game.PlaceId == 8951451142 then -- skywars
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/StaryLOL/Levitate/main/modules/skywars.lua'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/StaryLOL/Levitate/main/modules/skywars.lua", true))()
 
 elseif game.PlaceId == 11630038968 or game.PlaceId == 12011959048 then -- bridgeduels
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/StaryLOL/Levitate/main/modules/bridgeduels.lua'))()
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/StaryLOL/Levitate/main/modules/bridgeduels.lua", true))()
 
 else 
-
-local CoreGui = game:GetService("StarterGui")
-
-CoreGui:SetCore("SendNotification", {
-	Title = "Levitate";
-	Text = "Game not suported, join the discord for more info!";
-	Duration = 5;
-	Button1 = "Dismiss";
-})
-
+ print("not supported")
 end
