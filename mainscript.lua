e037419d7ac794d48609c208a804a5ea3d692544
Star <125512934+StaryLOL@users.noreply.github.com>
2024-04-21 15:25:14 -0700

    Update mainscript.lua

--- mainscript.lua
+++ mainscript.lua
@@ -1,6 +1,3 @@
-local CoreGui = game:GetService("StarterGui")
-
-
 loadstring(game:HttpGet('https://raw.githubusercontent.com/StaryLOL/Levitate/Bin/antikick.lua'))()) -- bridgeduels kick bypass
    wait(1.5)
 
@@ -8,27 +5,15 @@ if game.PlaceId == 8542259458 or game.PlaceId == 8542275097 or game.PlaceId == 8
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/StaryLOL/Levitate/modules/skywars.lua'))()
 
-CoreGui:SetCore("SendNotification", {
-	Title = "Levitate";
-	Text = "Levitate loaded! | Skywars";
-	Duration = 5;
-	Button1 = "Dismiss";
-})
-
 elseif game.PlaceId == 11630038968 or game.PlaceId == 12011959048 then -- bridgeduels
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/StaryLOL/Levitate/main/modules/bridgeduels.lua'))()
 
-CoreGui:SetCore("SendNotification", {
-	Title = "Levitate";
-	Text = "Levitate loaded! | Bridgeduels";
-	Duration = 5;
-	Button1 = "Dismiss";
-})
-
 
 else 
- 
+
+local CoreGui = game:GetService("StarterGui")
+
 CoreGui:SetCore("SendNotification", {
 	Title = "Levitate";
 	Text = "Game not suported, join the discord for more info!";
